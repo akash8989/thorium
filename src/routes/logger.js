f7ad80e1962c8c53008485be099e533304589298
akash singh <akashcool271995>
2022-02-22 10:46:51 +0530

    my first push

---
+++ src/routes/logger.js
@@ -0,0 +1,4 @@
+function printMessage(){
+    console.log(  'Welcome to my application. I am akash kumar and a part of functionup cohort.'   )
+}
+module.exports.printMessage = printMessage
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const log=require('./logger')
 router.get('/test-me', function (req, res) {
+  log.printMessage()
     res.send('My first ever api!')
 });
 
